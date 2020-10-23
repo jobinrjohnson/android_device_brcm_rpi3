3b95b35e2c9bc16ab8f43376b95390514a304ff2
Jobin Johnson <jobinrjohnson@gmail.com>
2020-10-23 07:29:35 +0000

    rpi: add correct kernelimage name and toolchain prefix

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,7 +15,6 @@ TARGET_AUX_OS_VARIANT_LIST := neonkey
 
 TARGET_NO_BOOTLOADER := true
 
-
 # Kernel
 TARGET_NO_KERNEL         := false
 TARGET_KERNEL_ARCH       := arm64
@@ -23,9 +22,10 @@ BOARD_KERNEL_CMDLINE     := console=ttyS0,115200
 BOARD_KERNEL_BASE        := 0x10000000
 BOARD_MKBOOTIMG_ARGS     := --kernel_offset 0x080000 --ramdisk_offset 0x01000000
 BOARD_KERNEL_PAGESIZE    := 2048
-BOARD_KERNEL_IMAGE_NAME  := zImage
+BOARD_KERNEL_IMAGE_NAME  := Image
 TARGET_KERNEL_CONFIG 	 := bcmrpi3_defconfig
 TARGET_KERNEL_SOURCE 	 := kernel/brcm/rpi3
+KERNEL_TOOLCHAIN_PREFIX  := aarch64-linux-androidkernel-
 
 
 # Audio
